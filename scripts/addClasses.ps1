df64a9a672d8e955a746bf39d106e931574af07e
dpierzak <dawid.pierzak@mccannworldgroup.pl>
2020-10-14 21:19:58 +0200

    fix powershell scripts

--- scripts/addClasses.ps1
+++ scripts/addClasses.ps1
@@ -1,27 +1,42 @@
-Install-module -name AzureAD
-Import-module -name AzureAD
+NetSH WinHTTP Import Proxy Source=IE
+Set-ExecutionPolicy RemoteSigned
 
-#connection
-netsh winhttp import proxy source=ie
-import-module exchangeonlinemanagement
-$username = $args[0]
-$password = $args[1]
-$domain = $args[2]
-$secstr = New-Object -TypeName System.Security.SecureString
-$password.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
-$O365Cred= new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $secstr
+#ModInstall
+Install-Module -Name MSOnline
+Install-Module -Name AzureAD
+Install-Module -Name ExchangeOnlineManagement
+
+#Modimport
+Import-Module -Name MSOnline
+Import-Module -Name AzureAD
+Import-Module -Name ExchangeOnlineManagement
+
+#Login & Sessions
+$Domain = $args[2]
+$Username = $args[0]
+$Password = $args[1]
+$SecSTR = New-Object -TypeName System.Security.SecureString
+$Password.ToCharArray() | ForEach-Object {$SecSTR.AppendChar($_)}
+$O365Cred= New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $SecSTR
 Connect-AzureAD -Credential $O365Cred
+Connect-MSOLService -Credential $O365Cred
+Connect-IPPSSession -ConnectionUri https://eur04b.ps.compliance.protection.outlook.com/Powershell-LiveId/ -Credential $O365Cred
+$EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $O365Cred -Authentication Basic -AllowRedirection
+$SCCSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $O365Cred -Authentication Basic -AllowRedirection
+Import-PSSession $EXOSession -AllowClobber
+Import-PSSession $SCCSession -AllowClobber
 
 #read csv
 $csv = Import-Csv -Path .\export\Groups.csv -Delimiter ','
 $csv | Format-Table
 ForEach($group in $csv){
-    $check = Get-UnifiedGroup -Identity $group.alias+$domain
+    $alias = $group.alias+$domain
+    $check = Get-UnifiedGroup -Identity $alias
     if($check){
         #do nothing
     } else {
         #create new group
-    	New-UnifiedGroup -DisplayName $group.name -Alias $group.alias -EmailAddresses $group.alias+$domain -AccessType Private
+    	New-UnifiedGroup -DisplayName $group.name -Alias $group.alias -EmailAddresses $alias -AccessType Private
     	Write-Host 'Added group '+$group.name
     }
 
--- scripts/add_users.ps1
+++ scripts/add_users.ps1
@@ -1,25 +1,39 @@
-Install-module -name AzureAD
-Import-module -name AzureAD
+NetSH WinHTTP Import Proxy Source=IE
+Set-ExecutionPolicy RemoteSigned
 
-#connection
-netsh winhttp import proxy source=ie
-import-module exchangeonlinemanagement
-$username = $args[0]
-$password = $args[1]
-$domain = $args[2]
-$secstr = New-Object -TypeName System.Security.SecureString
-$password.ToCharArray() | ForEach-Object {$secstr.AppendChar($_)}
-$O365Cred= new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $secstr 
+#ModInstall
+Install-Module -Name MSOnline
+Install-Module -Name AzureAD
+Install-Module -Name ExchangeOnlineManagement
+
+#Modimport
+Import-Module -Name MSOnline
+Import-Module -Name AzureAD
+Import-Module -Name ExchangeOnlineManagement
+
+#Login & Sessions
+$Domain = $args[2]
+$Username = $args[0]
+$Password = $args[1]
+$SecSTR = New-Object -TypeName System.Security.SecureString
+$Password.ToCharArray() | ForEach-Object {$SecSTR.AppendChar($_)}
+$O365Cred= New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $SecSTR
 Connect-AzureAD -Credential $O365Cred
-
-#Create new default password for users
-
+Connect-MSOLService -Credential $O365Cred
+Connect-IPPSSession -ConnectionUri https://eur04b.ps.compliance.protection.outlook.com/Powershell-LiveId/ -Credential $O365Cred
+$EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $O365Cred -Authentication Basic -AllowRedirection
+$SCCSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $O365Cred -Authentication Basic -AllowRedirection
+Import-PSSession $EXOSession -AllowClobber
+Import-PSSession $SCCSession -AllowClobber
 
 #read csv
 $csv = Import-Csv -Path .\export\Students.csv -Delimiter ','
 $csv | Format-Table
 ForEach($user in $csv){
-	$groups = $user.groups.Split("*")
+	$DisplayName = $user.name + ' ' + $user.surname
+	$UPN = $user.surname + '.' + $user.name + $domain
+    $MailNickName = $user.Name + ' ' + $user.Surname
+
 	$check = Get-AzureADUser -ObjectId $user.surname+'.'+$user.name+$domain
 	if($check){
 	#USER EXISTS
@@ -29,17 +43,22 @@ ForEach($user in $csv){
 	#USER DOESN'T EXIST
         #$PasswordProfile=New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
         #$PasswordProfile.Password=$defaultPassword
-		New-AzureADUser -DisplayName $user.name+' '+$user.surname -GivenName $user.name -SurName $user.surname -UserPrincipalName $user.surname+'.'+$user.name+$domain -UsageLocation "PL"	-MailNickName $user.name+' '+$user.surname -PasswordProfile $user.password -AccountEnabled $true
-		Write-Host 'Added user '+$user.name+' '+$user.surname
-        #enroll user to new groups
-		ForEach($groups in $group){
-			Add-UnifiedGroupLinks –Identity $group.alias+$domain –LinkType "Members" –Links $user.surname+'.'+$user.name+$domain
-			Write-Host 'Added user '+$user.name+' '+$user.surname+' to group: '+$group.alias
-		}
-
+        New-AzureADUser -DisplayName $DisplayName -GivenName $user.name -SurName $user.urname -UserPrincipalName $UPN -UsageLocation "Pl" -MailNickName $MailNickName -PasswordProfile $user.password -AccountEnabled $True
+        Write-Host 'Added user '+$user.name+' '+$user.surname
 	}
-	
-	
+}
+#give a minute for M$ to process users
+Start-Sleep -seconds 90
+
+#Enroll users
+ForEach($user in $csv){
+    $groups = $user.groups.Split("*")
+    $user = $user.surname + '.' + $user.name + $domain
+    ForEach($group in groups){
+        $gp = $group + $domain
+        Add-UnifiedGroupLinks –Identity $gp –LinkType "Members" –Links $user
+        Write-Host "Added user" $user "to group:" $gp
+    }
 }
 
 
